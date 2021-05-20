0eb21c3edc5d218fa4a097033787085cabfd0aa1
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-05-20 20:50:57 -0300

    react-modal added and first modal created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 function App() {
+  const [NewTransactionModal, setNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setNewTransactionModal(true);
+  }
+  
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+      
+      <Modal 
+          isOpen={NewTransactionModal} 
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Register transaction</h2>
+      </Modal>  
     </>
   );
 }
