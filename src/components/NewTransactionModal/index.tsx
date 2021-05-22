7278a6722dbb8614c921d5d11100dbf1029d907d
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-05-22 19:44:37 -0300

    Form implemented, but data not going to api yet

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomingImg from '../../assets/incoming.svg';
@@ -11,7 +11,21 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState('');
   const [transactionType, setTransactionType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      title,
+      amount,
+      category,
+      transactionType
+    })
+  }
 
   return (
     <Modal 
@@ -27,16 +41,21 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
       >
         <img src={closeImg} alt="Close" />
       </button>
-      <Container>
+      
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Record transaction</h2>
         
         <input 
           placeholder="Title" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
         />
         
         <input 
           type="number"
           placeholder="Amount" 
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
         
         <TransactionTypeContainer>
@@ -62,7 +81,9 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
         </TransactionTypeContainer>
 
         <input 
-          placeholder="Type" 
+          placeholder="Category" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}
         />
         
         <button type="submit">Record</button>
